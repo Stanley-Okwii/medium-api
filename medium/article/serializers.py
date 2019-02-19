b6a95d729d20d55cf9ce4bf356113e152fd96744
Stanley-Okwii <stanleeparker12@gmail.com>
2019-02-19 16:50:36 +0300

    [New] project setup

---
+++ medium/article/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+
+from .models import Article
+
+
+class ArticleSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=120)
+    description = serializers.CharField()
+    body = serializers.CharField()
+    author_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.body = validated_data.get('body', instance.body)
+        instance.author_id = validated_data.get('author_id', instance.author_id)
+
+        instance.save()
+        return instance
+
+    def put(self, request, pk):
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
+        data = request.data.get('article')
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({"success": "Article '{}' updated successfully".format(article_saved.title)})
+
+    def delete(self, request, pk):
+        # Get object with this pk
+        article = get_object_or_404(Article.objects.all(), pk=pk)
+        article.delete()
+        return Response({"message": "Article with id `{}` has been deleted.".format(pk)},status=204)
